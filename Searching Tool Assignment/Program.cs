b792d38ba7479dea61594dbadc8dae1b68e7019c
ndiakos <ndiakos@minedu.gov.gr>
2022-08-03 10:45:51 +0300

    - Swagger authentication added

--- Searching Tool Assignment/Program.cs
+++ Searching Tool Assignment/Program.cs
@@ -33,20 +33,15 @@ builder.Services.AddCors(options =>
 });
 
 //builder.Services.AddSwaggerGen();
-builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1", new OpenApiInfo
+builder.Services.AddSwaggerGen(c => 
     {
-        Title = "JWTToken_Auth_API",
-        Version = "v1"
-    });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
     {
         Name = "Authorization",
         Type = SecuritySchemeType.ApiKey,
         Scheme = "Bearer",
         BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+        In = ParameterLocation.Header
     });
     c.AddSecurityRequirement(new OpenApiSecurityRequirement {
         {
