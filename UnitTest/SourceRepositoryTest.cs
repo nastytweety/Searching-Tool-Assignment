cdfb97b52dfcb04e09076003c2873471e4d0f711
Nikos Diakos <ndiakos@minedu.gov.gr>
2024-09-24 13:07:13 +0300

    - added testing

---
+++ UnitTest/SourceRepositoryTest.cs
@@ -0,0 +1,15 @@
+using Searching_Tool_Assignment.Models;
+using Searching_Tool_Assignment.Repositories;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class SourceRepositoryTest
+    {
+        [TestMethod]
+        public void SourceExists_SourceNameExists_ReturnsTrue()
+        {
+
+        }
+    }
+}
